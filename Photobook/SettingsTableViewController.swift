b01c9f2012991c9f499d6641aecae306f9b90bc6
Fabian <fabiandemoel@gmail.com>
2019-05-07 10:11:57 +0200

    Commit

---
+++ Photobook/SettingsTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SettingsTableViewController.swift
+//  Photobook
+//
+//  Created by Fabian de Moel on 05/05/2019.
+//  Copyright © 2019 Fabian de Moel. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    // Switch user profile
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
